e8193ac412d442e57f69231bd8f56815fbc62092
easyimpr <ecygbsj@163.com>
2025-06-01 18:33:31 +0800

    修改未成功,加油，i can do it

--- handler/get_weather_make_clouthes.go
+++ handler/get_weather_make_clouthes.go
@@ -2,9 +2,12 @@ package handler
 
 import (
 	"ai_clouthes_backed/config"
+	"ai_clouthes_backed/utils"
+	"encoding/json"
 	"fmt"
 	"github.com/dingdinglz/vivo"
 	"github.com/gofiber/fiber/v3"
+	"os"
 )
 
 func MakeWeatherClouthes(ctx fiber.Ctx) error {
@@ -14,8 +17,15 @@ func MakeWeatherClouthes(ctx fiber.Ctx) error {
 		AppID:  config.Configs.Vivo.AppID,
 		AppKey: config.Configs.Vivo.AppKey,
 	})
-
-	say := fmt.Sprintf("我是一名%s性，根据%s的天气,为我推荐合适的日常穿着。严格按以下JSON格式返回：{\"ClothingMatch\": {\"UpperGarment\": \"具体上装推荐\",\"Bottoms\": \"具体下装推荐\"}}要求：1. 仅返回JSON对象，不要包含任何解释性文字2. 推荐需符合天气和性别3. 可自由修改推荐内容，但保持字段结构不变4. 衣物描述需具体明确（如材质/厚度/款式）", sex, weather)
+	content, e := os.ReadFile("content.txt")
+	if e != nil {
+		utils.Logger.Error("ReadFile error" + e.Error())
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 2052,
+			"msg":  "根据天气推荐衣服失败，请稍后再试",
+		})
+	}
+	say := fmt.Sprintf("我是一名%s性，根据%s的天气,为我推荐合适的日常穿着。严格按以下JSON格式返回：%s", sex, weather, string(content))
 	res, err := app.Chat(vivo.GenerateRequestID(), vivo.GenerateSessionID(), []vivo.ChatMessage{
 		{
 			Role:    vivo.CHAT_ROLE_USER,
@@ -23,14 +33,40 @@ func MakeWeatherClouthes(ctx fiber.Ctx) error {
 		},
 	}, nil)
 	if err != nil {
+		utils.Logger.Error("Chat error" + err.Error())
 		ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"code": 2051,
 			"msg":  "根据天气推荐衣服失败，请稍后再试",
 		})
 	}
+	var jsonString string
+	if err := json.Unmarshal([]byte(res.Content), &jsonString); err != nil {
+		utils.Logger.Error("Unmarshal json error" + err.Error() + res.Content)
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 2053,
+			"msg":  "根据天气推荐衣服失败，请稍后再试",
+		})
+	}
+	type ClothingMatch struct {
+		UpperGarment string `json:"UpperGarment"`
+		Bottoms      string `json:"Bottoms"`
+	}
+
+	type Response struct {
+		ClothingMatch ClothingMatch `json:"ClothingMatch"`
+	}
+
+	var data Response
+	if err := json.Unmarshal([]byte(jsonString), &data); err != nil {
+		utils.Logger.Error("Unmarshal json error" + err.Error())
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": 2054,
+			"msg":  "根据天气推荐衣服失败，请稍后再试",
+		})
+	}
 	return ctx.JSON(fiber.Map{
 		"code": 1000,
-		"data": res,
+		"data": data,
 		"msg":  "success",
 	})
 }
